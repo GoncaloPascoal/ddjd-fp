96949aaf59981e886ec787ac4b892e149d107fd0
Gonçalo Pascoal <goncalojpascoal@gmail.com>
2022-06-08 11:08:54 +0100

    Add Stats class
    Former-commit-id: 62ad11e202ff53e7fae36c16ab0ac5e99778e7ff [formerly e4c6b2bce3efebcb30b0ae9da27013444355fea0]
    Former-commit-id: 7ed8187991fb67b3cf0e58f1a7e806c438370756

---
+++ Assets/Scripts/Stats.cs
@@ -0,0 +1,12 @@
+
+using System;
+
+[Serializable]
+public class Stats
+{
+    public int baseHealth;
+    public float baseStamina, baseStaminaRecovery;
+    public int baseDamage;
+    public int baseArmor;
+    public int baseStability;
+}
