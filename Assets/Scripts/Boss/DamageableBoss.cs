98ed5798e40378e6a875f5d13a1f04cf3af11470
davide-castro20 <davideafc@gmail.com>
2022-06-14 18:03:28 +0100

    Fixed health bar
    Former-commit-id: f0b51a4540a3355390c47c1e86a1061cab8b86bd [formerly c2055992e42a2f91d83af2276c56133ae7634688]
    Former-commit-id: 9dfd1e9d2b06c0f8f79655620d3bfd2fa2d9233f

--- Assets/Scripts/Boss/DamageableBoss.cs
+++ Assets/Scripts/Boss/DamageableBoss.cs
@@ -4,20 +4,8 @@ using UnityEngine;
 
 public class DamageableBoss : Damageable
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     protected override void Die()
     {
         throw new System.NotImplementedException();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
