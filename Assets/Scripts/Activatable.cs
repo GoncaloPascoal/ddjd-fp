9ea3595db33a886d901fe092604b9a4a37bf30f7
Diogo <diogogrosario1008@gmail.com>
2022-06-14 21:39:05 +0100

    Door and pressure Plate
    Former-commit-id: 54047b54a7d66497cf8884e1c7c105c60fac80ee [formerly 35ea3daeefc65f9d5339e69972c225ec91d48368]
    Former-commit-id: 46aa09a33431c86106e377f1e56c6d47cf2bfeab

---
+++ Assets/Scripts/Activatable.cs
@@ -0,0 +1,10 @@
+using System;
+using UnityEngine;
+
+public interface Activatable
+{
+
+    public void activate();
+
+    public void deactivate();
+}
---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Transactions;
+using StarterAssets;
+using UnityEngine;
+
+public class Door : MonoBehaviour, Activatable
+{
+
+    private Animator _animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void activate()
+    {
+        Debug.Log("Activate");
+        _animator.SetTrigger("Open");
+    }
+
+    public void deactivate()
+    {
+        
+    }
+}
