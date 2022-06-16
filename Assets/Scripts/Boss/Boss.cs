9b33ad5f042902366e40a03e7fb3611c539193c0
davide-castro20 <davideafc@gmail.com>
2022-06-16 15:57:53 +0100

    Boss deals damage to player with each swing
    Former-commit-id: 773e72b9839387cd46423603be1363b5a0faa8f8 [formerly 7d17404d7cc55e09fec5b1ce587ec33f7be28f5e]
    Former-commit-id: aaa173331dfcac2716f94f4ed15dc42c0f452f34

--- Assets/Scripts/Boss/Boss.cs
+++ Assets/Scripts/Boss/Boss.cs
@@ -7,17 +7,60 @@ public class Boss : MonoBehaviour
     [SerializeField] private int _maxHealth;
 
     private Damageable _damageable;
+
+    private Animator _animator;
+    private Attacker _attacker;
+
+    public float defaultAttackCooldown = 2f;
+    
+    private float _attackCooldown;
     
     // Start is called before the first frame update
     void Start()
     {
         _damageable = GetComponent<Damageable>();
         _damageable.InitializeMaxHealth(_maxHealth);
+
+        _animator = GetComponent<Animator>();
+        _attacker = GetComponent<Attacker>();
+
+        _attackCooldown = defaultAttackCooldown;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void Attack()
+    {
+        _attacker.AttackNotBuffered(new List<string> {"Multiple Slashes"});
+    }
+
+    public void LookAtTarget(Vector3 target)
+    {
+        Quaternion lookRotation = Quaternion.Euler(transform.rotation.eulerAngles.x,
+            Quaternion.LookRotation(target - transform.position).eulerAngles.y,
+            transform.rotation.eulerAngles.z);
+        transform.rotation =
+            Quaternion.Lerp(transform.rotation, lookRotation,
+                Time.deltaTime * 5f);
+    }
+
+    public float GetAttackCooldown()
+    {
+        return _attackCooldown;
+    }
+
+    public float ReduceAttackCooldown()
+    {
+        _attackCooldown -= Time.deltaTime;
+        return _attackCooldown;
+    }
+    
+    public void SetAttackCooldown(float cooldown)
+    {
+        _attackCooldown = cooldown;
     }
 }
