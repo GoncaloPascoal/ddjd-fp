0165325ed5e005e3a94e0caf40de0238d891475f
Gonçalo Pascoal <goncalojpascoal@gmail.com>
2022-06-21 18:31:31 +0100

    Display messages in HUD

--- Assets/Scripts/UI/HUD.cs
+++ Assets/Scripts/UI/HUD.cs
@@ -7,18 +7,20 @@ using UnityEngine;
 public class HUD : MonoBehaviour
 {
     [SerializeField] public Bar healthBar, staminaBar;
-    [SerializeField] private GameObject buttonPrompt;
+    [SerializeField] private GameObject buttonPrompt, messageDisplay;
     [SerializeField] private ItemPickupDisplay itemPickup;
 
     public static HUD Instance;
 
-    private TMP_Text _buttonPromptText;
-    private readonly Stack<string> _buttonPrompts = new Stack<string>();
+    private TMP_Text _buttonPromptText, _messageDisplayText;
+    private readonly Stack<string> _buttonPrompts = new Stack<string>(),
+        _messages = new Stack<string>();
 
     private void Awake()
     {
         Instance = this;
         _buttonPromptText = buttonPrompt.GetComponentInChildren<TMP_Text>();
+        _messageDisplayText = messageDisplay.GetComponentInChildren<TMP_Text>();
     }
 
     public void ShowButtonPrompt(IEnumerable<string> buttons, string action)
@@ -50,5 +52,30 @@ public class HUD : MonoBehaviour
     public void ShowItemPickup(ItemPickupDictionary items, float duration = 4f)
     {
         itemPickup.ShowItemPickup(items, duration);
+    }
+
+    public void ShowMessage(string message)
+    {
+        _messages.Push(message);
+        UpdateMessage();
+    }
+
+    public void HideMessage()
+    {
+        _messages.Pop();
+        UpdateMessage();
+    }
+
+    private void UpdateMessage()
+    {
+        if (_messages.Count == 0)
+        {
+            messageDisplay.SetActive(false);
+        }
+        else
+        {
+            messageDisplay.SetActive(true);
+            _messageDisplayText.SetText(_messages.Peek());
+        }
     }
 }
