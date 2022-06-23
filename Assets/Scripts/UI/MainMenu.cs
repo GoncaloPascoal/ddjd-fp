98abd642f919b57d843f8abbd7f00da4182ca065
Gonçalo Pascoal <goncalojpascoal@gmail.com>
2022-06-23 15:29:50 +0100

    Finish settings menu
    Former-commit-id: 82e115fa9a32c5102fafbab52bd8f6a4bc7b9854 [formerly 33778dda15836bdaf0bbbc19a3e703de07dbb8de]
    Former-commit-id: 9237a19219ea7ce3e849a2e260c45ac0fb33966a

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
@@ -11,6 +12,7 @@ public class MainMenu : MonoBehaviour
 
     private void Start()
     {
+        newGameButton.onClick.AddListener(() => SceneManager.LoadScene("Level1"));
         settingsButton.onClick.AddListener(() => settingsMenu.SetActive(true));
         quitButton.onClick.AddListener(Application.Quit);
     }
