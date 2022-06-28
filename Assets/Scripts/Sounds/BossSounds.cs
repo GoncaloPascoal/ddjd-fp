bb97b8d69fd3ca30cd15e5528f1abd15fc1468dc
Henrique Ribeiro <gamingwithapotato@gmail.com>
2022-06-28 19:50:08 +0100

    Added sounds to the boss
    Former-commit-id: f2834cb394b9b802dde1e2b7684273015ac4e97d [formerly e9d6b23491553981c7b2aa80dfc6d3dd029ee782]
    Former-commit-id: 4a42c17c0d1c545a0de3ec87a9fd727f0bcecb34

---
+++ Assets/Scripts/Sounds/BossSounds.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class BossSounds : EntitySounds
+{
+    [SerializeField] private int numberSwordAttacks = 4;
+    [SerializeField] private int numberSlams = 3;
+    [SerializeField] private int numberProjectile = 4;
+
+    [SerializeField]private List<String> parts = new List<string>{"body", "legs", "sword"};
+
+    public void SwordAttackSound()
+    {
+        int randomSound = Random.Range(1, numberSwordAttacks + 1);
+        
+        Play3DSound("boss/Sword/boss_sword_" + randomSound);
+    }
+    
+    public void SwordSlamSound()
+    {
+        int randomSound = Random.Range(1, numberSlams + 1);
+        
+        Play3DSound("boss/Sword_slam/slam_" + randomSound);
+    }
+    
+    public void ProjectileSound()
+    {
+        int randomSound = Random.Range(1, numberProjectile + 1);
+        Play3DSound("Boss/projectiles/projectile_" + randomSound);
+    }
+        
+    public void SummonSound()
+    {
+        Play3DSound("Boss/Summons/Summon_spell");
+    }
+    
+    public void ScreamSound()
+    {
+        Play3DSound("Boss/Boss_scream");
+    }
+}
